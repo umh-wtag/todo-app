67351f50740fc562b70949e274f61e7801a04f30
rails udemy course <syeda.sara@welldev.io>
2023-08-14 15:25:20 +0600

    step 1

---
+++ src/redux/reducers/todosReducer.js
@@ -0,0 +1,20 @@
+export const initialState = {
+    todosItems: [
+        {
+        text: 'Eat',
+        completed: true
+        },
+        {
+            text: 'Sleep',
+            completed: false
+        }
+    ],
+    visibilityFiler: 'SHOW_ALL'
+}
+
+export default function todosReducer(state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
---
+++ src/redux/store/index.js
@@ -0,0 +1,9 @@
+import thunk from "redux-thunk"
+import { composeWithDevTools } from "redux-devtools-extension"
+import { legacy_createStore as createStore, applyMiddleware } from "redux"
+import rootReducer from "../reducers"
+
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+)
