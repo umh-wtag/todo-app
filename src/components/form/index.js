d5fafe27501cd3e210cdfbb4e1af0ac0388bdbed
rails udemy course <syeda.sara@welldev.io>
2023-08-16 11:29:37 +0600

    add todo form style change

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,33 +1,35 @@
-import React ,{useState} from "react";
+import React, { useState } from "react"
 import styles from "./form.module.css"
-import check from "../../utils/checkmark.png"
-import { useDispatch} from "react-redux";
-import { AddNewTodo } from "../../redux/actions";
+import trash from "../../utils/trash.png"
+import { useDispatch } from "react-redux"
+import { AddNewTodo } from "../../redux/actions"
 
 function Form() {
   const dispatch = useDispatch()
-  const [task, setTask] = useState('')
+  const [task, setTask] = useState("")
 
   function handleSubmit(e) {
     e.preventDefault()
     dispatch(AddNewTodo(task))
-    setTask('')
+    setTask("")
   }
-    return (
-      <main className={styles.card}>
-        <form className={styles.container}>
-          <input type="text"
-            name="body"
-            placeholder="Add Todo"
-            value={task}
-            onChange={(e)=>setTask(e.target.value)}
-          />
-          <div className={styles.actions} onClick={handleSubmit}>
-            <img src={check} alt="Done" className={styles.img} />
-          </div>
-        </form>
-      </main>
-    )
+  return (
+    <main className={styles.card}>
+      <form className={styles.container} onSubmit={handleSubmit}>
+        <textarea
+          className={styles.textarea}
+          name="body"
+          placeholder="Add Todo"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <div className={styles.actions}>
+          <button>Add Task</button>
+          <img src={trash} alt="Delete" className={styles.img} />
+        </div>
+      </form>
+    </main>
+  )
 }
 
 export default Form
