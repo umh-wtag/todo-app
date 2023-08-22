8752b6ad5bee5481887e5ae91c5e8b439406f1f9
rails udemy course <syeda.sara@welldev.io>
2023-08-22 13:52:29 +0600

    show completed

--- src/components/todoItem/index.js
+++ src/components/todoItem/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { useDispatch } from "react-redux"
-import { deleteTodo , markCompleted} from "redux/actions"
+import { deleteTodo, markCompleted } from "redux/actions"
 import PropTypes from "prop-types"
 import "components/todoItem/todoItem.scss"
 import {
@@ -14,27 +14,37 @@ function TodoItem({ todo }) {
   const dispatch = useDispatch()
   return (
     <div className="item_card">
-      <h2>{todo.text}</h2>
+      <h2 className={todo.completed === true && 'completed'}>{todo.text}</h2>
       <div className="item_card__bottom">
         <p className="item_card__time"> Created At : {todo.createdAt}</p>
         <div className="item_card__actions">
-          <img
-            src={IMAGE_PATHS.CHECK}
-            alt={COMPLETE_BUTTON_ALT}
-            className="item_card__actions__img"
-            onClick={() => dispatch(markCompleted(todo.text))}
-          />
-          <img
-            src={IMAGE_PATHS.EDIT}
-            alt={EDIT_BUTTON_ALT}
-            className="item_card__actions__img"
-          />
-          <img
-            src={IMAGE_PATHS.DELETE}
-            alt={DELETE_BUTTON_ALT}
-            className="item_card__actions__img"
-            onClick={() => dispatch(deleteTodo(todo.text))}
-          />
+          {todo.completed === false && (
+            <div className="item_card__actions">
+              <img
+                src={IMAGE_PATHS.CHECK}
+                alt={COMPLETE_BUTTON_ALT}
+                className="item_card__actions__img"
+                onClick={() => dispatch(markCompleted(todo.text))}
+              />
+              <img
+                src={IMAGE_PATHS.EDIT}
+                alt={EDIT_BUTTON_ALT}
+                className="item_card__actions__img"
+              />
+            </div>
+          )}
+          <div className="item_card__actions--completed">
+            <div>
+              <img
+                src={IMAGE_PATHS.DELETE}
+                alt={DELETE_BUTTON_ALT}
+                className="item_card__actions__img"
+                onClick={() => dispatch(deleteTodo(todo.text))}
+              />
+            </div>
+
+            <div>{todo.completed === true && <p> Completed </p>}</div>
+          </div>
         </div>
       </div>
     </div>
