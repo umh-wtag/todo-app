d6c7668b81e9c0d64191c13cf898c93c5e56778c
rails udemy course <syeda.sara@welldev.io>
2023-08-22 09:39:01 +0600

    remove redundant divs

--- src/components/todoList/index.js
+++ src/components/todoList/index.js
@@ -3,9 +3,9 @@ import { useSelector } from "react-redux"
 import TodoItem from "components/todoItem"
 import AddTodoForm from "components/addTodoForm"
 import "components/todoList/todoList.scss"
-import { IMAGE_PATHS } from "utils/constants"
-const selectTodos = (state) => state.todos.todoItems
+import { IMAGE_PATHS, ADD_FIRST_TASK } from "utils/constants"
 
+const selectTodos = (state) => state.todos.todoItems
 const TodoList = () => {
   const todos = useSelector(selectTodos)
   const selectIsAdding = (state) => state.todos.isAdding
@@ -21,7 +21,7 @@ const TodoList = () => {
     <div>
       {todos.length < 1 && toggleIsAdding === false && (
         <div className="add">
-          <img src={IMAGE_PATHS.ADD} alt="Add First task" className="add__img" />
+          <img src={IMAGE_PATHS.ADD} alt={ADD_FIRST_TASK} />
           <h1>You didn't add any task. Please, add one.</h1>
         </div>
       )}
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 import pencil from "assets/pencil.png"
-import trash from 'assets/trash.png'
-import done from 'assets/checkmark.png'
+import trash from "assets/trash.png"
+import done from "assets/checkmark.png"
 import add from "assets/undraw_add_files_re_v09g 1.png"
 import search from "assets/search.png"
 import vector from "assets/Vector.png"
@@ -9,7 +9,8 @@ export const COMPLETE_BUTTON_ALT = "Complete Button"
 export const EDIT_BUTTON_ALT = "Edit Button"
 export const DELETE_BUTTON_ALT = "Delete Button"
 export const SEARCH_ALT = "Search"
-export const LOGO = 'Brand_logo'
+export const LOGO = "Brand_logo"
+export const ADD_FIRST_TASK = "Add First Task"
 
 export const IMAGE_PATHS = {
   EDIT: pencil,
@@ -17,5 +18,5 @@ export const IMAGE_PATHS = {
   CHECK: done,
   ADD: add,
   ICON_SEARCH: search,
-  BRAND_LOGO: vector
+  BRAND_LOGO: vector,
 }
