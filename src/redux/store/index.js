e9641bf71e739fc010c73813f255df0bf5c836b9
rails udemy course <syeda.sara@welldev.io>
2023-08-24 14:12:21 +0600

    import order change

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,6 +1,6 @@
+import { legacy_createStore as createStore, applyMiddleware } from "redux"
 import thunk from "redux-thunk"
 import { composeWithDevTools } from "redux-devtools-extension"
-import { legacy_createStore as createStore, applyMiddleware } from "redux"
 import rootReducer from "redux/reducers"
 
 export const store = createStore(
