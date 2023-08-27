5f87b04704de59bdd2096f2c8e68d64f6a0ce0dd
umh-wtag <139974355+umh-wtag@users.noreply.github.com>
2023-08-27 14:48:54 +0600

    code formatted

--- src/components/buttons/index.js
+++ src/components/buttons/index.js
@@ -1,26 +1,14 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { useDispatch } from "react-redux"
-import { isAdding } from "redux/actions"
-import { useSelector } from "react-redux"
 
+function Button({ text, onClick }) {
 
-function Button({ text }) {
-  const dispatch = useDispatch()
-  const selectIsAdding = (state) => state.todos.isAdding
-  const toggleIsAdding = useSelector(selectIsAdding)
-
-  function handleClick() {
-    if (text === "Create") {
-      dispatch(isAdding(!toggleIsAdding))
-    }
-  }
-  
-  return <button onClick={handleClick}>{text}</button>
+  return <button onClick={onClick}>{text}</button>
 }
 
 Button.propTypes = {
   text: PropTypes.string.isRequired,
+  onClick: PropTypes.func
 }
 
 export default Button
