c7cfec233ae6cbbda8beffa1e7b66e9a667f0887
umh-wtag <139974355+umh-wtag@users.noreply.github.com>
2023-08-27 14:15:26 +0600

    add id

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,3 +1,4 @@
+import { v4 as uuid } from 'uuid'
 import { ADD_TODO, IS_ADDING } from "redux/actions/actionTypes"
 import { formatedDate } from "utils/formatedDate"
 
@@ -5,6 +6,7 @@ export const addNewTodo = (todo) => {
   return {
     type: ADD_TODO,
     payload: {
+      id: uuid(),
       text: todo,
       createdAt: formatedDate(new Date()),
       completed: false,
