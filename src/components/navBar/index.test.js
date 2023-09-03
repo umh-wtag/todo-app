682190cf5dfc51c02f4f38f79302696535b9d4d8
umh-wtag <139974355+umh-wtag@users.noreply.github.com>
2023-09-03 11:21:11 +0600

    navbar test

--- src/components/navBar/index.test.js
+++ src/components/navBar/index.test.js
@@ -1,8 +1,12 @@
-import { render , screen } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
+import "@testing-library/jest-dom"
 import NavBar from "components/navBar"
 
 it("renders the navBar component", () => {
     render(<NavBar />)
     const navbarContainer = screen.getByTestId('navbar-container')
+    const todosHeader = screen.getByText("Todos")
+    
+    expect(todosHeader).toBeInTheDocument()
     expect(navbarContainer).not.toBeNull()
 })
