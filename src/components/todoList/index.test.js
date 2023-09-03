76f9162725035434fde9f1516864fe2a5180893a
umh-wtag <139974355+umh-wtag@users.noreply.github.com>
2023-09-03 10:20:45 +0600

    format

--- src/components/todoList/index.test.js
+++ src/components/todoList/index.test.js
@@ -13,36 +13,36 @@ const initialState = {
     isAdding: false,
   },
 }
-let store;
+let store
 beforeEach(() => {
   store = mockStore(initialState)
 })
 
-it('renders TodoList component', () => {
+it("renders TodoList component", () => {
   render(
     <Provider store={store}>
       <TodoList />
     </Provider>
   )
 
-  const card = screen.queryAllByTestId('item-card')
+  const card = screen.queryAllByTestId("item-card")
   expect(card).not.toBeNull()
 })
 
-it('displays addTodoForm when isAdding is true', () => {
+it("displays addTodoForm when isAdding is true", () => {
   store = mockStore({
     ...initialState,
     todos: {
       ...initialState.todos,
-      isAdding: true
-    }
+      isAdding: true,
+    },
   })
   render(
     <Provider store={store}>
-      <TodoList/>
+      <TodoList />
     </Provider>
   )
-  const todoForm = screen.getByTestId('add-todo-form')
+  const todoForm = screen.getByTestId("add-todo-form")
   expect(todoForm).not.toBeNull()
 })
 
@@ -52,12 +52,12 @@ it('displays "You didn\'t add any task when todos array is empty"', () => {
     todos: {
       ...initialState,
       todoItems: [],
-      isAdding: false
-    }
+      isAdding: false,
+    },
   })
   render(
-    <Provider store ={store}>
-      <TodoList/>
+    <Provider store={store}>
+      <TodoList />
     </Provider>
   )
   const emptyTaskMessage = screen.getByText(
